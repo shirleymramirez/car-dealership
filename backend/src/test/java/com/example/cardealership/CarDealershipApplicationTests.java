67151533f0b5da4c06f08e8358444fea71108f97
Shirley Ramirez <shirley.mramirez@yahoo.com>
2019-08-26 20:14:06 -0700

    First commit

---
+++ backend/src/test/java/com/example/cardealership/CarDealershipApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.cardealership;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarDealershipApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
